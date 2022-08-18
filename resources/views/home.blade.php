2d4cab9268e66ec2bbb31fa32a55a4fa9fbe877d
Renz Valenzuela <revalenzuelanz.029@gmail.com>
2022-08-18 16:56:25 +0800

    add laravel

---
+++ resources/views/home.blade.php
@@ -0,0 +1,41 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-3 p-5">
+                <img src="https://cdn.changelog.com/uploads/icons/news_sources/Qo/icon_small.png?v=63680075631"
+                    class="rounded-circle" alt="" height="100">
+            </div>
+            <div class="col-9 pt-5">
+                <div>
+                    <h1>iLux Website</h1>
+                </div>
+                <div class="d-flex">
+                    <div class="pe-5"><strong>153</strong> posts</div>
+                    <div class="pe-5"><strong>23k</strong> followers</div>
+                    <div class="pe-5"><strong>212</strong> following</div>
+                </div>
+                <div class="pt-4"><strong>ilux-solutions.com</strong></div>
+                <div>A bunch of random words here for the description.</div>
+                <div><a href="#">www.ilux-solutions.com</a></div>
+            </div>
+        </div>
+
+        <div class="row pt-5">
+            <div class="col-4 d-flex">
+                <img src="https://cdn.pixabay.com/photo/2022/05/08/10/13/animal-7181776_960_720.jpg" alt=""
+                    class="w-100">
+            </div>
+            <div class="col-4">
+                <img src="https://cdn.pixabay.com/photo/2022/07/25/18/47/waterfall-7344396_960_720.jpg" alt=""
+                    class="w-100">
+            </div>
+            <div class="col-4">
+                <img src="https://cdn.pixabay.com/photo/2022/07/29/01/07/trees-7350845_960_720.jpg" alt=""
+                    class="w-100">
+            </div>
+        </div>
+
+    </div>
+@endsection
