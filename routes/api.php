1626c7efdb6e957b394af64418706e8e695c2236
Renz Valenzuela <revalenzuelanz.029@gmail.com>
2022-08-20 14:00:42 +0800

    added post routes in api.php

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use GuzzleHttp\Client;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+
+Route::post('services', [App\Http\Controllers\TurboSMMController::class, 'services']);
+Route::post('balance', [App\Http\Controllers\TurboSMMController::class, 'balance']);
