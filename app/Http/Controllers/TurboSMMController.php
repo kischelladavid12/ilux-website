980494c6ac081fa0e9e770074966c32ef7ca336e
Renz Valenzuela <revalenzuelanz.029@gmail.com>
2022-08-23 22:55:19 +0800

    shortened functions for readability

--- app/Http/Controllers/TurboSMMController.php
+++ app/Http/Controllers/TurboSMMController.php
@@ -3,101 +3,35 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use GuzzleHttp\Client;
-use App\Models\Order;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Http;
+use App\Services\OrderAdd;
+use App\Services\ServiceList;
+use App\Services\Balance;
+use App\Services\OrderStatus;
 
 class TurboSMMController extends Controller
 {
-    private $api_url = 'https://turbosmmservices.com/api/v2';
 
-    private $api_key = '6629537ca795c0d31f2f1bd170d6628b';
-
-    public function services() //get services list
+    public function serviceList()
     {
-        return $this->connect($data = [
-            'key' => $this->api_key,
-            'action' => 'services'
-        ]);
+        $response = new ServiceList();
+        return $response->services();
     }
-
-
 
     public function balance()
     {
-        return $this->connect($data = [
-            'key' => $this->api_key,
-            'action' => 'balance'
-        ]);
+        $response = new Balance();
+        return $response->checkBal();
     }
-
-
 
     public function addOrder(Request $request,)
     {
-        $query = array();
-
-        foreach ($request->all() as $key => $value) {
-            if ($key != '_token' && $key != 'submit' && $key != 'package_name' && $key != 'price') {
-                $query[$key] = $value;
-            }
-        }
-
-        $post = array_merge(array(
-            'key' => $this->api_key,
-            'action' => 'add'
-        ), $query);
-
-        $response = $this->connect($post);
-
-        //Data insertion to local orders database
-        $orderCheck = $this->orderStatus($response->order);
-
-        $turbo_order_id = $response->order;
-        $turbo_service_id = $request->service;
-        $package_name = $request->package_name;
-        $price = $request->price;
-        $status = $orderCheck->status;
-
-
-        $isInsertSuccess = Order::insert([
-            'user_id' => Auth::user()->id,
-            'turbo_order_id' => $turbo_order_id,
-            'turbo_service_id' => $turbo_service_id,
-            'package_name' => $package_name,
-            'price' => $price,
-            'status' => $status
-        ]);
-
-        if ($isInsertSuccess) echo '<h1>Order Confirmed</h1>';
-        else echo '<h1>Order Failed</h1>';
-
-        return $response;
+        $response = new OrderAdd();
+        return $response->addOrder($request);
     }
 
-
-
-    public function orderStatus($orderId)
+    public function orderStatus($id)
     {
-        return $this->connect($data = [
-            'key' => $this->api_key,
-            'action' => 'status',
-            'order' => $orderId
-        ]);
-    }
-
-
-
-    private function connect($data)
-    {
-        $client = new Client(['base_uri' => $this->api_url]);
-        try {
-            $url = $this->api_url . '?' . http_build_query($data);
-            $response = Http::post($url);
-            return json_decode($response, true);
-        } catch (\GuzzleHttp\Exception\RequestException $e) {
-            echo $e->getRequest()->getUri();
-        }
+        $response = new OrderStatus();
+        return $response->orderStatus($id);
     }
 }
