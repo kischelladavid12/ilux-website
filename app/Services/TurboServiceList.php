48d2e4ca6df7e92e89fe87ce5f5c6dcdc5ee170d
Renz Valenzuela <revalenzuelanz.029@gmail.com>
2022-08-25 13:05:55 +0800

    minor edits

--- app/Services/TurboServiceList.php
+++ app/Services/TurboServiceList.php
@@ -9,6 +9,7 @@ class TurboServiceList
     public function services()
     {
         $connect_estab = new TurboConnect();
+
         return $connect_estab->connect($data = [
             'key' => env('API_KEY'),
             'action' => 'services'
