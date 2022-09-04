cbd4a60586af030b6a70b9611997b96008aeffb8
Renz Valenzuela <revalenzuelanz.029@gmail.com>
2022-09-04 21:25:06 +0800

    route groups and implemented admin middleware

--- routes/api.php
+++ routes/api.php
@@ -2,8 +2,10 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Auth\Access\Gate;
 use App\Http\Controllers\TurboSMMController;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\WalletController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,18 +22,33 @@ use App\Http\Controllers\AuthController;
 //public routes
 Route::post('/auth/register', [AuthController::class, 'register'])->name("/auth/register");
 Route::post('/auth/login', [AuthController::class, 'login']);
+Route::get('/auth/logout', [AuthController::class, 'logout']);
 
 
 //protected routes
 Route::group(['middleware' => ['auth:sanctum']], function () {
-
-    Route::get('/user', function (Request $request) {
-        return $request->user();
+    //user routes
+    Route::group([
+        'prefix' => 'user',
+        'as' => 'user.'
+    ], function () {
+        Route::get('/user', function (Request $request) {
+            return $request->user();
+        });
+        Route::post('balance', [WalletController::class, 'showBalance']);
+        Route::post('order-status/{id}', [TurboSMMController::class, 'orderStatus']);
     });
 
-    Route::get('/auth/logout', [AuthController::class, 'logout']);
 
-    Route::post('/services', [TurboSMMController::class, 'serviceList']);
-    Route::post('balance', [TurboSMMController::class, 'balance']);
-    Route::post('order-status/{id}', [TurboSMMController::class, 'orderStatus']);
+    //admin routes
+    Route::group([
+        'prefix' => 'admin',
+        'middleware' => 'is_admin',
+        'as' => 'admin.'
+    ], function () {
+        Route::post('add-bal', [WalletController::class, 'add']);
+        Route::post('deduct-bal', [WalletController::class, 'deduct']);
+        Route::post('turbo-services', [TurboSMMController::class, 'serviceList']);
+        Route::post('turbo-balance', [TurboSMMController::class, 'balance']);
+    });
 });
