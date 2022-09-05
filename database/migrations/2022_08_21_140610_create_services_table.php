8694e3da61b364708c7b2f73652f81cbedb27e17
Renz Valenzuela <revalenzuelanz.029@gmail.com>
2022-09-06 00:31:54 +0800

    added table relationship

--- database/migrations/2022_08_21_140610_create_services_table.php
+++ database/migrations/2022_08_21_140610_create_services_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('services', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('service_name');
+            $table->string('service_name')->unique();
             $table->bigInteger('turbo_service_id');
             $table->float('price');
             $table->timestamps();
