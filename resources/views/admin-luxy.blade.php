b2c9dc54e3ec70303e6ad2268c3b2257ab3b7458
jermainepilar <jermainerochelle.pilar@cvsu.edu.ph>
2022-09-05 09:36:04 +0800

    wallet list updated

--- resources/views/admin-luxy.blade.php
+++ resources/views/admin-luxy.blade.php
@@ -31,7 +31,7 @@
         <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
             <!-- Sidebar - Brand -->
-            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="/">
+            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="/admin">
                 <div class="sidebar-brand-text mx-3">iLux</div>
             </a>
 
@@ -40,7 +40,7 @@
 
             <!-- Nav Item - Dashboard -->
             <li class="nav-item active">
-                <a class="nav-link" href="/">
+                <a class="nav-link" href="/admin">
                     <i class="fas fa-fw fa-tachometer-alt"></i>
                     <span>Admin</span></a>
             </li>
--- resources/views/admin-wallet.blade.php
+++ resources/views/admin-wallet.blade.php
@@ -151,6 +151,42 @@
 
                     <!-- Page Heading -->
                     <h1 class="h3 mb-2 text-gray-800">Wallet List</h1>
+                    <button type="button" class="btn btn-primary" data-toggle="modal"
+                        data-target="#exampleModalCenter">
+                        Add Amount
+                    </button><br>
+
+                    <!-- Modal -->
+                    <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog"
+                        aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+                        <div class="modal-dialog modal-dialog-centered" role="document">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h5 class="modal-title" id="exampleModalLongTitle">Add Amount</h5>
+                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                        <span aria-hidden="true">&times;</span>
+                                    </button>
+                                </div>
+                                <div class="modal-body">
+                                    <form class="user">
+                                        <div class="form-group">
+                                            <input type="text" class="form-control form-control-user"
+                                                id="adminKey" name="$adminKey" placeholder="Admin Key">
+                                            <input type="text" class="form-control form-control-user"
+                                                id="user_id" name="$user_id" placeholder="User ID">
+                                            <input type="number" class="form-control form-control-user"
+                                                id="amount"name="$amount" placeholder="Amount to be Added">
+                                        </div>
+                                    </form>
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary"
+                                        data-dismiss="modal">Close</button>
+                                    <button type="button" class="btn btn-primary">Save changes</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
 
                     <!-- DataTales Example -->
                     <div class="card shadow mb-4">
