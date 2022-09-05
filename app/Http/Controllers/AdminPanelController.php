68e5cbfe6f0bf8dc394db54c71a6e838e30562b8
Renz Valenzuela <revalenzuelanz.029@gmail.com>
2022-09-05 22:55:16 +0800

    added admin functions

--- app/Http/Controllers/AdminPanelController.php
+++ app/Http/Controllers/AdminPanelController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use App\Models\Service;
 
 class AdminPanelController extends Controller
 {
@@ -15,8 +16,8 @@ class AdminPanelController extends Controller
     public function displayOrders()
     {
         try {
-            $users = DB::table('orders')->orderBy('updated_at', 'DESC')->get();
-            return $users;
+            $query = DB::table('orders')->orderBy('updated_at', 'DESC')->get();
+            return $query;
         } catch (\Throwable $th) {
             return response()->json([
                 'status' => false,
@@ -28,8 +29,34 @@ class AdminPanelController extends Controller
     public function displayWallets()
     {
         try {
-            $users = DB::table('wallets')->orderBy('updated_at', 'DESC')->get();
-            return $users;
+            $query = DB::table('wallets')->orderBy('updated_at', 'DESC')->get();
+            return $query;
+        } catch (\Throwable $th) {
+            return response()->json([
+                'status' => false,
+                'message' => $th->getMessage(),
+            ], 500);
+        }
+    }
+
+    public function displayServices()
+    {
+        try {
+            $query = DB::table('services')->orderBy('updated_at', 'DESC')->get();
+            return $query;
+        } catch (\Throwable $th) {
+            return response()->json([
+                'status' => false,
+                'message' => $th->getMessage(),
+            ], 500);
+        }
+    }
+
+    public function displayUsers()
+    {
+        try {
+            $query = DB::table('users')->select('id', 'username', 'email', 'created_at', 'updated_at')->orderBy('updated_at', 'DESC')->get();
+            return $query;
         } catch (\Throwable $th) {
             return response()->json([
                 'status' => false,
@@ -40,5 +67,60 @@ class AdminPanelController extends Controller
 
     public function findWallet(Request $request)
     {
+    }
+
+    public function createService(Request $request)
+    {
+        try {
+            Service::create([
+                'service_name' => $request->service_name,
+                'turbo_service_id' => $request->turbo_service_id,
+                'price' => $request->price
+            ]);
+
+            return response()->json([
+                'status' => true,
+                'message' => 'Service Creation Success!'
+            ]);
+        } catch (\Throwable $th) {
+            return response()->json([
+                'status' => false,
+                'message' => $th->getMessage(),
+            ], 500);
+        }
+    }
+
+    public function updateTServID(Request $request)
+    {
+        try {
+            Service::where('id', $request->id)->update(array('turbo_service_id' => $request->turbo_service_id));
+
+            return response()->json([
+                'status' => true,
+                'message' => 'Turbo Service ID Updated!'
+            ]);
+        } catch (\Throwable $th) {
+            return response()->json([
+                'status' => false,
+                'message' => $th->getMessage(),
+            ], 500);
+        }
+    }
+
+    public function updatePrice(Request $request)
+    {
+        try {
+            Service::where('id', $request->id)->update(array('price' => $request->price));
+
+            return response()->json([
+                'status' => true,
+                'message' => 'Price Updated!'
+            ]);
+        } catch (\Throwable $th) {
+            return response()->json([
+                'status' => false,
+                'message' => $th->getMessage(),
+            ], 500);
+        }
     }
 }
