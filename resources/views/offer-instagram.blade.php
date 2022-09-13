2da7c68c203ba0beff7d811866f3144960b59172
Kisses <kizzesdavid@gmail.com>
2022-09-13 14:54:01 +0800

    modal title

--- resources/views/offer-instagram.blade.php
+++ resources/views/offer-instagram.blade.php
@@ -164,7 +164,7 @@
                                             <div class="modal-dialog modal-dialog-centered">
                                                 <div class="modal-content">
                                                     <div class="modal-header">
-                                                        <h5 class="modal-title" id="instagramModalLabel">INSTAGRAM
+                                                        <h5 class="modal-title" id="instagramModalLabel">
                                                         </h5>
                                                         <button type="button" class="close" data-dismiss="modal"
                                                             aria-label="Close">
--- resources/views/offer-tiktok.blade.php
+++ resources/views/offer-tiktok.blade.php
@@ -164,7 +164,7 @@
                                             <div class="modal-dialog modal-dialog-centered">
                                                 <div class="modal-content">
                                                     <div class="modal-header">
-                                                        <h5 class="modal-title" id="tiktokModalLabel">TIKTOK</h5>
+                                                        <h5 class="modal-title" id="tiktokModalLabel"></h5>
                                                         <button type="button" class="close" data-dismiss="modal"
                                                             aria-label="Close">
                                                             <span aria-hidden="true">&times;</span>
